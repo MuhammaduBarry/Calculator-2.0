71f9697f1cfd5bd401c03c6a87baa8a4f0a2a1c8
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-02-28 21:04:52 -0500

    finished ui

--- Calculator 2.0/main.js
+++ Calculator 2.0/main.js
@@ -1,1 +1,21 @@
+const numbers = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".operation");
+const mainControl = document.querySelectorAll(".main-control");
 
+document.addEventListener("click", (e) => {
+  for (let i = 0; i < numbers.length; i++) {
+    if (e.target === numbers[i]) {
+      console.log(numbers[i].value);
+    }
+  }
+  for (let i = 0; i < operations.length; i++) {
+    if (e.target === operations[i]) {
+      console.log(operations[i].value);
+    }
+  }
+  for (let i = 0; i < mainControl.length; i++) {
+    if (e.target === mainControl[i]) {
+      console.log(e.target);
+    }
+  }
+});
