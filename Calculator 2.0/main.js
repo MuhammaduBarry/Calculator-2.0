4eddb080df0e0189a2d210cb7e733f34de707372
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-03-02 08:51:52 -0500

    test

--- Calculator 2.0/main.js
+++ Calculator 2.0/main.js
@@ -1,21 +1,17 @@
-const numbers = document.querySelectorAll(".number");
-const operations = document.querySelectorAll(".operation");
-const mainControl = document.querySelectorAll(".main-control");
+const calculatorButtons = document.querySelectorAll(
+  ".number, .operation, .main-control"
+);
 
-document.addEventListener("click", (e) => {
-  for (let i = 0; i < numbers.length; i++) {
-    if (e.target === numbers[i]) {
-      console.log(numbers[i].value);
+const test = () => {
+  for (let i = 0; i < calculatorButtons.length; i++) {
+    const button = calculatorButtons[i];
+
+    if (button.classList.contains("number")) {
+      console.log(button.value);
+    } else if (button.classList.contains("operation")) {
+      console.log(button.value);
+    } else if (button.classList.contains("main-control")) {
+      console.log(button.value);
     }
   }
-  for (let i = 0; i < operations.length; i++) {
-    if (e.target === operations[i]) {
-      console.log(operations[i].value);
-    }
-  }
-  for (let i = 0; i < mainControl.length; i++) {
-    if (e.target === mainControl[i]) {
-      console.log(e.target);
-    }
-  }
-});
+};
