5a5922c3eb8994419b2933ef202f2e79237cd794
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-03-02 13:33:04 -0500

    updating functionality

--- Calculator 2.0/main.js
+++ Calculator 2.0/main.js
@@ -1,17 +1,30 @@
 const calculatorButtons = document.querySelectorAll(
   ".number, .operation, .main-control"
 );
+const calculatorContainer = document.querySelector("#calculator-container");
+let calculatorInput = document.querySelector("#calculator-input");
 
-const test = () => {
-  for (let i = 0; i < calculatorButtons.length; i++) {
-    const button = calculatorButtons[i];
+// Creating a string to hold our values
+let firstNumberString = "";
 
-    if (button.classList.contains("number")) {
-      console.log(button.value);
-    } else if (button.classList.contains("operation")) {
-      console.log(button.value);
-    } else if (button.classList.contains("main-control")) {
-      console.log(button.value);
+// We can only have one decimal click, so we use this to check
+let canClickDecimal = true;
+
+// This allows us to hold our values
+const firstNumber = (e) => {
+  let count = 0;
+  // This allows us to to only click on number classes
+  const clickedNumber = e.target.classList.contains("number");
+  if (clickedNumber) {
+    if (e.target.value == "." && canClickDecimal) {
+      firstNumberString += e.target.value;
+      canClickDecimal = false;
+    } else if (e.target.value !== ".") {
+      firstNumberString += e.target.value;
     }
   }
+
+  console.log(firstNumberString);
 };
+
+calculatorContainer.addEventListener("click", firstNumber);
