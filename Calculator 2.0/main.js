e4ea4e6327d6e37a299ac1ae66db08a77206b767
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-03-02 17:41:12 -0500

    Finished number conditions

--- Calculator 2.0/main.js
+++ Calculator 2.0/main.js
@@ -1,30 +1,67 @@
-const calculatorButtons = document.querySelectorAll(
-  ".number, .operation, .main-control"
-);
 const calculatorContainer = document.querySelector("#calculator-container");
 let calculatorInput = document.querySelector("#calculator-input");
-
-// Creating a string to hold our values
-let firstNumberString = "";
-
-// We can only have one decimal click, so we use this to check
 let canClickDecimal = true;
+let isNumberComplete = false;
+let OperatorClicked = false;
 
-// This allows us to hold our values
+let firstNumberString = "";
+let secondNumberString = "";
+
 const firstNumber = (e) => {
-  let count = 0;
   // This allows us to to only click on number classes
   const clickedNumber = e.target.classList.contains("number");
+
   if (clickedNumber) {
-    if (e.target.value == "." && canClickDecimal) {
+    // We don't want our zeros to continuously stack :)
+    if (firstNumberString === "0") {
+      // Handle the case where the first number is 0
+      firstNumberString = e.target.value;
+    } else if (e.target.value == "." && canClickDecimal) {
       firstNumberString += e.target.value;
       canClickDecimal = false;
+      isNumberComplete = true;
     } else if (e.target.value !== ".") {
       firstNumberString += e.target.value;
+      isNumberComplete = true;
     }
+    calculatorInput.placeholder = firstNumberString;
+    console.log(`This is first number: ${firstNumberString}`);
   }
-
-  console.log(firstNumberString);
 };
 
 calculatorContainer.addEventListener("click", firstNumber);
+
+const operator = (e) => {
+  const clickedOperator = e.target.classList.contains("operation");
+
+  if (clickedOperator) {
+    calculatorInput.placeholder = e.target.value;
+    calculatorContainer.removeEventListener("click", firstNumber);
+    console.log(`This is my operator: ${calculatorInput.placeholder}`);
+    OperatorClicked = true;
+  }
+};
+
+calculatorContainer.addEventListener("click", operator);
+
+const secondNumber = (e) => {
+  const clickedNumber = e.target.classList.contains("number");
+
+  if (clickedNumber && OperatorClicked) {
+    if (firstNumberString === "0") {
+      // Handle the case where the first number is 0
+      firstNumberString = e.target.value;
+    } else if (e.target.value == "." && canClickDecimal) {
+      firstNumberString += e.target.value;
+      canClickDecimal = false;
+      isNumberComplete = true;
+    } else if (e.target.value !== ".") {
+      firstNumberString += e.target.value;
+      isNumberComplete = true;
+    }
+    calculatorInput.placeholder = firstNumberString;
+    console.log(`This is first number: ${firstNumberString}`);
+  }
+};
+
+calculatorContainer.addEventListener("click", secondNumber);
